cb64f0ed0b28524f7d7fa934cb8745bfff6c49b9
Ibrahim3zazy <Ibrahim3zazy@hotmail.com>
2022-02-21 20:13:43 +0200

    update 1.0

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function play_vid() {
+  let video = document.getElementById('play_vid');
+  document.getElementById('autoplay').setAttribute('src', 'media/proj_intro.mp4');
+  video.load();
+}
+function unMute() {
+  let video = document.getElementById('play_vid');
+  video.muted = !video.muted;
+  document.getElementById('unMute').style.display = "none";
+}
+function skip() {
+  document.getElementById('play_vid').style.transform = "translate(-70%, -50%)";
+  document.getElementById('skip').style.transform = "translateX(-170%)";
+  document.getElementsByClassName('header')[0].style.top = "0%";
+  document.getElementById('play_vid').style.animationName = "none";
+  document.getElementById('skip').style.animationName = "none";
+  document.getElementsByClassName('header')[0].style.animationName = "none";
+}
